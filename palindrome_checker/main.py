0972a26cc1b31ebb5c6784f9861e9c351a7d39ab
srek09 <ignacz.andras1109@gmail.com>
2024-05-24 09:30:47 +0200

    palindrome_checker first try

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -1,4 +1,20 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
+import re
 
-print(input)
+pattern = re.compile('\W')
+with open('./input.txt', 'r', encoding='utf-8') as f:
+  input = f.readlines()
+
+for line in input:
+  line = re.sub(pattern, '', line).lower().strip()
+  unique_chars = len(set(line))
+  p1 = 0
+  p2 = len(line) - 1
+  is_palindrome = True
+  while p1 <= p2:
+    if line[p1] != line[p2]:
+      is_palindrome = False
+      unique_chars = -1
+      break
+    p1 += 1
+    p2 -= 1
+  print(f"{is_palindrome and 'YES' or 'NO'}, {unique_chars}")
