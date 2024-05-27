4558c3859afe5727444cc59909c9be7253b7a960
srek09 <ignacz.andras1109@gmail.com>
2024-05-27 19:34:18 +0200

    matr | docs.2

--- matrix_operations/main.py
+++ matrix_operations/main.py
@@ -1,10 +1,15 @@
-matrices = {}
-operations = []
-operators = {'+': 1, '*': 2}
+matrices = {} # read from input file
+operations = [] # read from input file
+operators = {'+': 1, '*': 2} #operators allowed
 
+# matrix addition
+# we go through each element of the matrix and add the corresponding elements of the two matrices
 def add(A, B):
 	return [[A[i][j] + B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
 
+
+# matrix multiplication
+# we go through each element of the matrix and multiply the corresponding (col = row, row = col) elements of the two matrices
 def multiply(A, B):
 	return [[sum (A[i][k] * B[k][j] for k in range(len(A[0]))) for j in range(len(B[0]))] for i in range(len(A))]
 
@@ -19,32 +24,45 @@ with open('./input.txt', 'r') as f:
 			current_line += 2
 			line = input[current_line].strip()
 
-			while line != 'operations':
+			while line != 'operations': # read all matrices
 				id = line
 				current_line += 1
 				line = input[current_line].strip()
 				matrix = []
+
 
 				while len(line) > 0 and line[0].isdigit():
 					matrix.append([int(num) for num in line.split()])
 					current_line += 1
 					line = input[current_line].strip()
 
+
 				matrices[id] = matrix
 				current_line += 1
 				line = input[current_line].strip()
+    
 			current_line += 2
 
 		operations.append(input[current_line].strip())
 		current_line += 1
 
 
+# Shunting Yard Algorithm
+# we convert the infix expression to postfix
+# we use a stack to store the operators and a queue to store the operands
+# we go through each token in the expression and check if it is an operator or an operand
+# if it is an operator we check if it has higher precedence than the operator on top of the stack
+# if it has higher precedence we add it to the stack
+# if it has lower precedence we pop the operator from the stack and add it to the queue
+# if it is an operand we add it to the queue
+# we add the remaining operators from the stack to the queue
+# we evaluate the postfix expression
+
 def shunting_yard(expression):
 	output_queue = []
 	operator_stack = [] 
 
 	for token in expression.replace(' ', ''):
-
 		if token in operators:
 			while (operator_stack and operators[token] <= operators[operator_stack[-1]]):
 				output_queue.append(operator_stack.pop())
@@ -58,6 +76,7 @@ def shunting_yard(expression):
 	return output_queue
 
 
+# here we calculate the result of the postfix expression
 def evaluate_postfix(expression):
 	stack = []
 	for token in expression:
@@ -72,6 +91,7 @@ def evaluate_postfix(expression):
 	return stack and stack.pop() or ''
 
 
+# evaluating and printing the results
 for operation in operations:
 	result = evaluate_postfix(shunting_yard(operation))
 	print(operation, '\n'.join([' '.join(str(j) for j in i) for i in result]), sep='\n', end='\n\n')
